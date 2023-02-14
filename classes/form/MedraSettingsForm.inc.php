35d5ad1903ccc3ca0534374cf6ed92cd442c1878
Bozana Bokan <bozana.bokan@posteo.net>
2023-02-14 15:58:44 +0100

    adapt copyright year, increase version

--- classes/form/MedraSettingsForm.inc.php
+++ classes/form/MedraSettingsForm.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file plugins/importexport/medra/classes/form/MedraSettingsForm.inc.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2003-2021 John Willinsky
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2003-2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class MedraSettingsForm
--- filter/IssueMedraXmlFilter.inc.php
+++ filter/IssueMedraXmlFilter.inc.php
@@ -3,8 +3,8 @@
 /**
  * @file plugins/importexport/medra/filter/IssueMedraXmlFilter.inc.php
  *
- * Copyright (c) 2014-2021 Simon Fraser University
- * Copyright (c) 2000-2021 John Willinsky
+ * Copyright (c) 2014-2023 Simon Fraser University
+ * Copyright (c) 2000-2023 John Willinsky
  * Distributed under the GNU GPL v3. For full terms see the file docs/COPYING.
  *
  * @class IssueMedraXmlFilter
