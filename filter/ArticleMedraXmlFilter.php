656022897452c4a6d5522c1dc1026766e26eddb3
Jehu Njikonga <39902417+njikonga-med@users.noreply.github.com>
2025-10-20 18:30:35 +0200

    Refactor access rights and keyword handling
    Refactor access rights handling and keyword processing logic. Simplify issue retrieval and improve access indicators creation.

--- filter/ArticleMedraXmlFilter.php
+++ filter/ArticleMedraXmlFilter.php
@@ -218,6 +218,38 @@ class ArticleMedraXmlFilter extends O4DOIXmlFilter
             $this->appendTextMiningCollectionNodes($doc, $articleNode, $article, $submissionGalleys);
         }
 
+        $issueId = $article->getCurrentPublication()->getData('issueId');
+        if ($cache->isCached('issues', $issueId)) {
+            /** @var Issue $issue */
+            $issue = $cache->get('issues', $issueId);
+        } else {
+            $issue = Repo::issue()->get($issueId, $context->getId());
+            if ($issue) {
+                $cache->add($issue, null);
+            }
+        }
+        $accessRights = null;
+        if($context->getData('publishingMode') == PUBLISHING_MODE_OPEN){
+                $accessRights = 'openAccess';
+        } else if($context->getData('publishingMode') == PUBLISHING_MODE_SUBSCRIPTION) {
+                if ($issue->getAccessStatus() == ISSUE_ACCESS_OPEN) {
+                        $accessRights = 'openAccess';
+                } else if ($article->getCurrentPublication()->getData('accessStatus') == ARTICLE_ACCESS_OPEN) {
+                                $accessRights = 'openAccess';
+                        }
+        }
+        $rightsURL = $article->getCurrentPublication()->getData('licenseUrl') ?? $context->getData('licenseUrl');
+        if($accessRights == 'openAccess' || !empty($rightsURL)){
+                $accessIndicatorsNode = $doc->createElementNS($deployment->getNamespace(), 'AccessIndicators');
+                if($accessRights == 'openAccess'){
+                        $accessIndicatorsNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'FreeToRead'));
+                }
+                if(!empty($rightsURL)){
+                        $accessIndicatorsNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'License', $rightsURL));
+                }
+                $articleNode->appendChild($accessIndicatorsNode);
+        }
+
         // DOI structural type
         $articleNode->appendChild($node = $doc->createElementNS($deployment->getNamespace(), 'DOIStructuralType', $this->getDOIStructuralType()));
 
@@ -241,16 +273,6 @@ class ArticleMedraXmlFilter extends O4DOIXmlFilter
         $articleNode->appendChild($this->createSerialPublicationNode($doc, $journalLocalePrecedence, $epubFormat));
 
         // Journal Issue (mandatory)
-        $issueId = $article->getCurrentPublication()->getData('issueId');
-        if ($cache->isCached('issues', $issueId)) {
-            /** @var Issue $issue */
-            $issue = $cache->get('issues', $issueId);
-        } else {
-            $issue = Repo::issue()->get($issueId, $context->getId());
-            if ($issue) {
-                $cache->add($issue, null);
-            }
-        }
         $articleNode->appendChild($this->createJournalIssueNode($doc, $issue, $journalLocalePrecedence));
 
         // Object locale precedence.
@@ -332,8 +354,9 @@ class ArticleMedraXmlFilter extends O4DOIXmlFilter
         $allKeywords = $article->getCurrentPublication()->getData('keywords');
         $keywords = $this->getPrimaryTranslation($allKeywords, $objectLocalePrecedence);
         if (!empty($keywords)) {
-            $keywordsString = implode(';', $keywords);
-            $contentItemNode->appendChild($this->createSubjectNode($doc, self::O4DOI_SUBJECT_SCHEME_KEYWORDS, $keywordsString));
+            foreach ($keywords as $keyword) {
+                $contentItemNode->appendChild($this->createSubjectNode($doc, self::O4DOI_SUBJECT_SCHEME_KEYWORDS, $keyword));
+            }
         }
 
         // Object Description 'OtherText'
